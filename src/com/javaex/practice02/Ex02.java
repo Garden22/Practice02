8d805a201152a3feaa47d00e54ee9bf649a04b12
gardenee <snnjjang@naver.com>
2022-04-15 19:21:13 +0900

    220415

---
+++ src/com/javaex/practice02/Ex02.java
@@ -0,0 +1,15 @@
+package com.javaex.practice02;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		int x = 0;
+		
+		//x가 0과 같으면
+		if (x == 0) {      // '='는 변수에 값 할당
+			System.out.println("x는 0이다.");
+			
+		}
+	}
+
+}
